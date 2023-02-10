1db6e85dc41f9b3de3b35865a906fe6643149bfe
Glindley23 <graham.lindley23@gmail.com>
2023-02-09 19:55:30 -0500

    added index

--- src/App.js
+++ src/App.js
@@ -9,5 +9,5 @@ function App() {
     </div>
   );
 }
-console.log('test')
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -12,9 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-app.listen(process.env.PORT)
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
