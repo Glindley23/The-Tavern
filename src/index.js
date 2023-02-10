0020ef3a63c02997d23cbd55dd4a30513f399c67
Glindley23 <graham.lindley23@gmail.com>
2023-02-09 19:35:07 -0500

    deleted placeholder code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +12,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+app.listen(process.env.PORT)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
