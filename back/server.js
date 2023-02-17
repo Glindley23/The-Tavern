0fab2a98e830e00b4029afc2b3eaef3dd7a479f7
Andrew <andrewcrink@gmail.com>
2023-02-16 19:07:26 -0500

    fixed index.js, database connects now

--- back/server.js
+++ back/server.js
@@ -1,9 +1,27 @@
 //Modules and Globals
 require('dotenv').config()
 const express = require('express');
+const methodOverride = require('method-override')
+const mongoose = require ('mongoose')
+const db = require('./models')
+
 const app = express();
+
 
 //PORT
 app.listen(process.env.PORT, () => {
-    console.log('listening on port 8080!')
+    console.log('listening on port 3001!')
 });
+
+app.get('/drinks', (req,res) => {
+    db.Drink.find()
+    .then((drinks) => {
+        res.status(200).json(drinks)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.status(400).json(err)
+    })
+})
+    
+    
