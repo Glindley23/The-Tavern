cdf232920c08dd140cf3db71c8510ab314b7c069
Melik Melikyan <mhmelikyan@gmail.com>
2023-02-16 19:35:26 -0500

    employee routs finished

--- back/controllers/employee.js
+++ back/controllers/employee.js
@@ -13,6 +13,34 @@ router.post('/', async (req, res) => {
     res.json(employee)
 })
 
+//get by id
+router.get('/employee/:id', async (req, res) => {
+    const employee = await Employee.findById(id)
+    const {id} = req.params
+    res.json(employee)
+  })
+
+   //PUT:
+   router.put('/:id', async(req,res)=>{
+    const { id }  = req.params
+     await Employee.findByIdAndUpdate(id, req.body)
+      res.redirect(`/employee/${id}`)
+  
+   })
+  
+   //POST:
+  router.post('/', async (req,res)=>{
+      await Employee.create(req.body)
+   res.redirect('/employee')
+  })
+      // Delete employee
+      router.delete('/:id', async (req,res)=> {
+          const { id } = req.params
+          await Employee.findByIdAndDelete(id)   
+      
+          res.status(303).redirect('/employee')
+      })
+      
 
 
 module.exports=router
