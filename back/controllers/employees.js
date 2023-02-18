67fcf5b95cbe3d3776936dfb17844d38625a4b93
Andrew <andrewcrink@gmail.com>
2023-02-17 22:08:29 -0500

    got get, get by id, and delete routes working, still working on put and push

--- back/controllers/employees.js
+++ back/controllers/employees.js
@@ -7,42 +7,63 @@ router.get('/', (req, res) => {
     db.Employee.find()
         .then((employees) => {
             res.send(employees)
-        }
-        )
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+          })
 })
 
-router.post('/', async (req, res) => {
-    const employees = await new Employees(req.body).save()
-    res.json(employees)
+router.post('/', (req, res) => {
+    db.Employee.create(req.body)
+    .then (() =>
+        res.redirect('/employees')
+) 
 })
+
 
 //get by id
-router.get('/employees/:id', async (req, res) => {
-    const employees = await Employees.findById(id)
-    const {id} = req.params
-    res.json(employees)
-  })
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await db.Employee.findById(req.params.id);
+    res.json(data)
+  }
+  catch(error) {
+    res.status(500).json({message: error.message})
+  }
+});
 
-   //PUT:
-   router.put('/:id', async(req,res)=>{
-    const { id }  = req.params
-     await Employees.findByIdAndUpdate(id, req.body)
-      res.redirect(`/employees/${id}`)
+//PUT:
+router.put('/:id', async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await db.Employee.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
   
    })
   
-   //POST:
-  router.post('/', async (req,res)=>{
-      await Employees.create(req.body)
-   res.redirect('/employees')
-  })
-      // Delete employee
+    // Delete employee
       router.delete('/:id', async (req,res)=> {
-          const { id } = req.params
-          await Employees.findByIdAndDelete(id)   
-      
-          res.status(303).redirect('/employees')
-      })
+        try {
+            const id = req.params.id;
+            const data = await db.Employee.findByIdAndDelete(id)
+            res.send(`Employee with ${data.name} has been deleted...`)
+        }
+        catch (error) {
+            res.status(400).json({ message: error.message})
+        }
+    })
+          
       
 
 
