d3676dbd9a8e7dcf4211a8e3725ab0cae1e17cf7
Glindley23 <graham.lindley23@gmail.com>
2023-02-17 21:53:55 -0500

    added navbar styling and brand logo

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 //Component Import
 import Home from './Components/Home';
 import Food from './Components/Food';
@@ -11,36 +12,60 @@ import Employees from './Components/Employees';
 import Equipment from './Components/Equipment';
 import Rooms from './Components/Rooms';
 import QuestBoard from './Components/QuestBoard';
+import './App.css';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+  crossorigin="anonymous"
+/>
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Container>
-          <Navbar bg="light" expand="lg">
-            <Nav className='Nav' defaultActiveKey='/'>
-                <Link to='/'>Home</Link>
-                <Link to='/employees'>Employees</Link>
-                <Link to='/food'>Food Inventory</Link>
-                <Link to='/drink'>Drink Inventory</Link>
-                <Link to='/equipment'>Equipment</Link>
-                <Link to='/rooms'>Rooms</Link>
-                <Link to='/questboard'>Quest Board</Link>
-            </Nav>
-          </Navbar>
-          </Container>
-          <div className='display'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/employees' element={<Employees />} />
-              <Route path='/food' element={<Food />} />
-              <Route path='/drink' element={<Drink />} />
-              <Route path='/equipment' element={<Equipment />} />
-              <Route path='/rooms' element={<Rooms />} />
-              <Route path='/questboard' element={<QuestBoard />} />
-            </Routes>
-          </div>
-        </Router>
+      <Navbar  bg="p-3 mb-2 bg-light bg-gradient rounded-2" variant='light'expand="lg">
+      <Container>
+        <Nav className=' justify-content-center navbar ' activeKey='/' >
+          <Navbar.Brand><img id='tavern_brand' src='/imgs/tavern_logo.png'/></Navbar.Brand>
+          <Nav.Item>
+            <Nav.Link href='/'>Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href='/employees'>Employees</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href='/food'>Food Inventory</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link to='/drink'>Drink Inventory</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link to='/equipment'>Equipment</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link to='/rooms'>Rooms</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link to='/questboard'>Quest Board</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        </Container>
+        </Navbar>
+
+        <div className='display'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/employees' element={<Employees />} />
+            <Route path='/food' element={<Food />} />
+            <Route path='/drink' element={<Drink />} />
+            <Route path='/equipment' element={<Equipment />} />
+            <Route path='/rooms' element={<Rooms />} />
+            <Route path='/questboard' element={<QuestBoard />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
 //const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 //const express = require('express')
