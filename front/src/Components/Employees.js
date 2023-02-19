0e4431be0623f52034730f3210d03026ca482d69
Glindley23 <graham.lindley23@gmail.com>
2023-02-18 19:07:44 -0500

    added employee view code - still needs api testing

--- front/src/Components/Employees.js
+++ front/src/Components/Employees.js
@@ -1,8 +1,47 @@
-function Employees () {
+import React, { useEffect, useState } from "react"
+
+function Employees() {
+    //setting state for employee data
+    const [employeesData, setEmployeesData] = useState([{}])
+    //fetching data from backend
+    useEffect(() => {
+        fetch('/employees').then(
+            response => response.json()
+        ).then(
+            data => {
+                setEmployeesData(data)
+            }
+        )
+    }, [])
+
+    let employeeList = employeesData.map((employee) => {
+        return (
+            <div className='col-sm-6'>
+                <h2>
+                    {employee.name}
+                </h2>
+                <p>
+                    {employee.job_title}
+                </p>
+                <img id='index-img' src={employee.portrait} alt={employee.portrait} />
+                <p>
+                    Years of experience: {employee.years_of_experience}
+                </p>
+                <p>
+                    Weekly Salary: {employee.weekly_salary}
+                </p>
+            </div>
+        )
+    })
+    //page render
     return (
-        <div>
-            <h1>Employee Management</h1>
-        </div>
+            <main>
+                <h1>Employee Management</h1>
+                <div className='row'>
+                    {employeeList}
+
+                </div>
+            </main>
     )
 }
 
--- front/src/Components/Home.js
+++ front/src/Components/Home.js
@@ -1,9 +1,11 @@
+
 function Home () {
     return (
         <div>
             <h1>The Tavern</h1>
             <img src='https://gamesartist.co.uk/wp-content/uploads/2021/03/Figure-1-2000x978.png' alt="tavern" />
         </div>
+        
     )
 }
 
