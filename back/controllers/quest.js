5c8fad429d6d95d0123eead90fdaaa98c49f78ef
Andrew <andrewcrink@gmail.com>
2023-02-18 22:11:39 -0500

    food, quest, and room controllers completed and functional

---
+++ back/controllers/quest.js
@@ -0,0 +1,77 @@
+const router = require('express').Router()
+const db= require('../models')
+
+//SHOW ALL Quests
+router.get('/', (req, res) => {
+    db.Quest.find()
+        .then((quests) => {
+            res.send(quests)
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+          })
+})
+
+//CREATE NEW Quest
+router.post('/', async (req, res) => {
+    const post = new db.Quest({
+        name: req.body.name,
+        description: req.body.description,
+        pay: req.body.pay,
+        status: req.body.status,
+    })
+    await db.Quest.create(post)
+    await post.save()
+    res.send(post)
+})
+
+//get quest by id
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await db.Quest.findById(req.params.id);
+    res.json(data)
+  }
+  catch(error) {
+    res.status(500).json({message: error.message})
+  }
+});
+
+//PUT: UPDATE Quest
+router.put('/:id', async(req,res)=>{
+   try {
+    const post = await db.Quest.findOne({ _id: req.params.id })
+    if (req.body.name) {
+        post.name = req.body.name
+    }
+    if (req.body.description) {
+        post.description = req.body.description
+    }
+    if (req.body.pay) {
+        post.pay = req.body.pay
+    }
+    if (req.body.status) {
+        post.status = req.body.status
+    }
+    await post.save()
+    res.send(post)
+    }   
+        catch {
+    res.status(404)
+    res.send({ error: "Post doesn't exist!" })
+}
+}) 
+  
+// Delete Quest
+router.delete('/:id', async (req,res)=> {
+    try {
+        const id = req.params.id;
+        const data = await db.Quest.findByIdAndDelete(id)
+        res.send(`Quest ${data.name} has been deleted...`)
+        }
+    catch (error) {
+        res.status(400).json({ message: error.message})
+        }
+    })
+
+module.exports = router
---
+++ back/controllers/room.js
@@ -0,0 +1,77 @@
+const router = require('express').Router()
+const db= require('../models')
+
+//SHOW ALL ROOMS
+router.get('/', (req, res) => {
+    db.Room.find()
+        .then((rooms) => {
+            res.send(rooms)
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+          })
+})
+
+//CREATE NEW ROOM
+router.post('/', async (req, res) => {
+    const post = new db.Room({
+        room: req.body.room,
+        available: req.body.available,
+        occupancy: req.body.occupancy,
+        cost_per_night: req.body.cost_per_night
+    })
+    await db.Room.create(post)
+    await post.save()
+    res.send(post)
+})
+
+//get room by id
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await db.Room.findById(req.params.id);
+    res.json(data)
+  }
+  catch(error) {
+    res.status(500).json({message: error.message})
+  }
+});
+
+//PUT: UPDATE Room
+router.put('/:id', async(req,res)=>{
+   try {
+    const post = await db.Room.findOne({ _id: req.params.id })
+    if (req.body.room) {
+        post.room = req.body.room
+    }
+    if (req.body.available) {
+        post.available = req.body.available
+    }
+    if (req.body.occupancy) {
+        post.occupancy = req.body.occupancy
+    }
+    if (req.body.cost_per_night) {
+        post.cost_per_night = req.body.cost_per_night
+    }
+    await post.save()
+    res.send(post)
+    }   
+        catch {
+    res.status(404)
+    res.send({ error: "Post doesn't exist!" })
+}
+}) 
+  
+// Delete Room
+router.delete('/:id', async (req,res)=> {
+    try {
+        const id = req.params.id;
+         const data = await db.Room.findByIdAndDelete(id)
+         res.send(`Room ${data.room} has been deleted...`)
+        }
+    catch (error) {
+            res.status(400).json({ message: error.message})
+        }
+    })
+
+module.exports = router
