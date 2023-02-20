0df5013fc1d2adb4d96194400aed534816c514bc
Andrew <andrewcrink@gmail.com>
2023-02-19 21:48:36 -0500

    fixed error

--- back/models/equipment.js
+++ back/models/equipment.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 let equipmentSchema = new mongoose.Schema({
-    name: { type: String},
-    type: { type: String},
-    cost_to_buy: { type: Number},
-    sell_price: { type: Number},
-    amount_in_stock: { type: Number }
+    name: { type: String, default: "name"},
+    type: { type: String, default: "type"},
+    cost_to_buy: { type: Number, default: 10},
+    sell_price: { type: Number, default: 12},
+    amount_in_stock: { type: Number, default: 10 }
 })
 
 module.exports = mongoose.model('Equipment', equipmentSchema)
